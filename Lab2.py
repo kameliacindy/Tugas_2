5f707d3770f7c51ab3da7c81ee3855380e0d1559
kameliacindy <kamelcindy60@gmail.com>
2019-10-28 09:35:30 +0700

    Update

---
+++ Lab2.py
@@ -0,0 +1,11 @@
+a = input("masukkan nilai a:")
+b = input("masukkan nilai b:")
+print("variabel a=", a)
+print("variabel b=", b)
+print('hasil penggabungan {}&{}'.format(a, b))
+
+#konversi nilai variable
+a = int(a)
+b = int(b)
+print("hasil penjumlahan {}+{}=%d".format(a,b) %(a+b))
+print("hasil pembagian {}/{}=%d".format(a,b) %(a/b))
